ff5c9e4de91bddc567829cc6fba42bd14b6ee89f
SerpDove <petersonola362@gmail.com>
2022-03-11 21:10:59 -0800

    my

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,45 +1,37 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of
- * three digits
- * Numbers must be separated by, followed by a space
- * The two digits must be different
- * 012,120,102,021,201,210 are considered the same, combination
- * Return: 0
+ * main - Prints combination of three digit numbers without repeating 0 1 and 2
+ *
+ * Return: Always 0 (Success)
  */
-
-/* betty style doc for function main goes there */
 
 int main(void)
 {
-	int n1 = 48;
-	int n2, n3;
-	int com = 44;
+    int tens, ones, hundreds;
 
-	while (n1 <= 57)
-		/* your code goes there */
-	{
-		n2 = n1 + 1;
-		while (n2 <= 57)
-		{
-			n3 = n2 + 1;
-			while (n3 <= 57)
-			{
-				putchar(n1);
-				putchar(n2);
-				putchar(n3);
-				if (n1 != 55 || n2 != 56 || n3 != 57)
-				{
-					putchar(com);
-					putchar(32);
-				}
-				n3 += 1;
-			}
-			n2 += 1;
-		}
-		n1 += 1;
-	}
-	putchar('\n')
-		return (0);
+    for (tens = '0'; tens < '9'; tens++)
+    {
+        for (ones = tens + 1; ones <= '9'; ones++)
+        {
+            for (hundreds = ones + 1; hundreds <= '9'; hundreds++)
+            {
+                if ((ones != tens) != hundreds)
+                {
+                    putchar(tens);
+                    putchar(ones);
+                    putchar(hundreds);
+
+                    if (tens == '7' && ones == '8')
+                        continue;
+
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+
+    return (0);
 }
